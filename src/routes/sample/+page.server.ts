1e5fb740222b70a68efc8d5db476bbca1ada50bf
shuichiro-hayafuji <shu1998hayafuji@gmail.com>
2024-03-04 13:14:13 +0900

    サンプルページを追加

--- src/routes/sample/+page.server.ts
+++ src/routes/sample/+page.server.ts
@@ -1,10 +1,23 @@
+import { getImageUrls } from "$lib/server/images/minioClient";
 import { getUserTable } from "../../lib/server/database/userInfo"
 
 export async function load() {
+    // DBからユーザー情報取得
     const userInfos = await getUserTable();
+    // MinIOから画像取得
+    const bucketName = "angels";
+    let images: Array<String> = [];
+    try {
+        images = await getImageUrls(bucketName);
+    } catch(error) {
+        console.error(error);
+        throw error
+    }
+    
     return {
         post: {
-            userInfos: JSON.stringify(userInfos)
+            userInfos: JSON.stringify(userInfos),
+            images: images
         }
     };
 }
